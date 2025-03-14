4dbce1cc2b164c4ee545b848dc9c352a919a2620
Guilherme Lara <115944884+devguilara@users.noreply.github.com>
2025-03-14 10:53:55 -0300

    Update index.jsx

--- app/components/homepage/projects/index.jsx
+++ app/components/homepage/projects/index.jsx
@@ -5,28 +5,29 @@ import { projectsData } from '@/utils/data/projects-data';
 import ProjectCard from './project-card';
 
 const Projects = () => {
-
   return (
-    <div id='projects' className="relative z-50  my-12 lg:my-24">
+    <div id="projects" className="relative z-50 my-12 lg:my-24">
+      {/* Cabeçalho */}
       <div className="sticky top-10">
-        <div className="w-[80px] h-[80px] bg-violet-100 rounded-full absolute -top-3 left-0 translate-x-1/2 filter blur-3xl  opacity-30"></div>
+        <div className="w-[80px] h-[80px] bg-violet-100 rounded-full absolute -top-3 left-0 translate-x-1/2 filter blur-3xl opacity-30"></div>
         <div className="flex items-center justify-start relative">
-          <span className="bg-[#1a1443] absolute left-0  w-fit text-white px-5 py-3 text-xl rounded-md">
+          <span className="bg-[#1a1443] absolute left-0 w-fit text-white px-5 py-3 text-xl rounded-md">
             Projetos
           </span>
           <span className="w-full h-[2px] bg-[#1a1443]"></span>
         </div>
       </div>
 
-      <div className="pt-24">
-        <div className="flex flex-col gap-6">
-          {projectsData.slice(0, 10).map((project, index) => (
+      {/* Lista de Projetos */}
+      <div className="pt-24 relative">
+        <div className="flex flex-col gap-8">
+          {projectsData.map((project, index) => (
             <div
-              id={`sticky-card-${index + 1}`}
+              id={`project-card-${index}`}
               key={index}
-              className="sticky-card w-full mx-auto max-w-2xl sticky"
+              className="w-full mx-auto max-w-2xl transform transition-all duration-500 hover:scale-105"
             >
-              <div className="box-border flex items-center justify-center rounded shadow-[0_0_30px_0_rgba(0,0,0,0.3)] transition-all duration-[0.5s]">
+              <div className="box-border flex items-center justify-center rounded shadow-lg bg-[#1a1443] p-6">
                 <ProjectCard project={project} />
               </div>
             </div>
